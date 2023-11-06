4975ad19166d3746d05d5c6d8f6f052b030034a9
Pulkit1822 <pulkitkumarmathur1@gmail.com>
2023-11-07 01:05:24 +0530

    new

--- CircleArea.java
+++ CircleArea.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class CircleArea {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter the radius of the circle: ");
-            double radius = scanner.nextDouble();
-            double area = Math.PI * radius * radius;
-            System.out.println("The area of the circle is: " + area);
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+        double radius = input.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.printf("The area of the circle is %.2f\n", area);
     }
 }
